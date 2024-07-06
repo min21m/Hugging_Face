46c755f8b25705649d6194c9845a7c7a1f6a72f3
min21m <101021260+min21m@users.noreply.github.com>
2024-07-06 07:02:20 -0500

    Initial Commit.

--- 2_Article_Recommend/Article_Recommend.py
+++ 2_Article_Recommend/Article_Recommend.py
@@ -1,1 +1,18 @@
-print ("Article Recommend")
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction import text
+from sklearn.metrics.pairwise import cosine_similarity
+data = pd.read_csv("C:/Users/amjoseph/Downloads/articles.csv", encoding='latin1')
+
+articles = data["Article"].tolist()
+
+uni_tfidf = text.TfidfVectorizer(input=articles, stop_words="english")
+
+uni_matrix = uni_tfidf.fit_transform(articles)
+
+uni_sim = cosine_similarity(uni_matrix)
+def recommend_articles(x):
+    return ", ".join(data["Title"].loc[x.argsort()[-5:-1]])
+data["Recommended Articles"] = [recommend_articles(x) for x in uni_sim]
+data.head()
+print(data["Recommended Articles"][22])
